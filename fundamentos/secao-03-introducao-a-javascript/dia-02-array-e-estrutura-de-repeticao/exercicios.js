75b235d5277fdbb4fe2b6dbe232e5730a4036a0a
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-07-02 17:14:48 -0300

    completando desafio fatorial

--- fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
@@ -2,12 +2,16 @@ const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //1 - Percorra o array imprimindo todos os valores contidos nele com a função console.log().
 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // for(i = 0; i < numbers.length; i += 1) {r
 //   const item = numbers[i];
 //   console.log(item);
 // }
 
 //2 - Some todos os valores contidos no array e imprima o resultado.
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // let sum = 0;
 
@@ -17,7 +21,9 @@ const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(sum);
 
-//3 - Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//3 & 4 -Calcule e imprima a média aritmética dos valores contidos no array. A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos. Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
 
 // let sum = 0;
 
@@ -27,7 +33,9 @@ const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // sum <= 20 ? console.log('O valor da média aritmética é menor ou igual a 20') : console.log('O valor da média aritmética é maior que 20');
 
-//4 - Utilizando for, descubra o maior valor contido no array e imprima-o.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//5 - Utilizando for, descubra o maior valor contido no array e imprima-o.
 
 // let bigerNumber = 0;
 
@@ -37,7 +45,9 @@ const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(bigerNumber);
 
-//5 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
 
 // let impar = 0;
 
@@ -46,3 +56,21 @@ const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // impar === 0 ? console.log("Nenhum valor ímpar encontrado") : console.log(impar);
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Desafio Fatorial - O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
+
+// O fatorial é representado pelo sinal !
+// Exemplo de 4 fatorial:
+// 4! = 4 x 3 x 2 x 1 = 24
+
+// Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+
+// let n = 10;
+
+// for(i = 9; i > 0; i -= 1) {
+//   n = n * i;
+// }
+
+// console.log(n);
