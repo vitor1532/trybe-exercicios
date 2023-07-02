4e2d572ea55399e7b020ee90ab60e778d2bfcced
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-07-02 16:54:37 -0300

    finalizando terceiro exercício

--- fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
@@ -16,3 +16,13 @@ const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // console.log(sum);
+
+//3 - Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+
+// let sum = 0;
+
+// for(i = 0; i < numbers.length; i += 1) {
+//   sum = sum += numbers[i];
+// }
+
+// sum <= 20 ? console.log('O valor da média aritmética é menor ou igual a 20') : console.log('O valor da média aritmética é maior que 20');
