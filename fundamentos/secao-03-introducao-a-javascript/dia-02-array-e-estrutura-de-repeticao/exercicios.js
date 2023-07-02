494995227ccf61fbcbf585bf58c3949ad3bb3724
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-07-02 16:59:06 -0300

    finalizando exercício 4

--- fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
@@ -26,3 +26,13 @@ const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // sum <= 20 ? console.log('O valor da média aritmética é menor ou igual a 20') : console.log('O valor da média aritmética é maior que 20');
+
+//4 - Utilizando for, descubra o maior valor contido no array e imprima-o.
+
+// let bigerNumber = 0;
+
+// for(i = 0; i < numbers.length; i += 1) {
+//   numbers[i] > bigerNumber ? bigerNumber = numbers[i] : bigerNumber;
+// }
+
+// console.log(bigerNumber);
