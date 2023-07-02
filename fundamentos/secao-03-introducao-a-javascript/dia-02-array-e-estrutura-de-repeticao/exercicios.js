4d50c8d89a75bc2dc49590efc6ab86e4550a98ee
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-07-02 16:46:30 -0300

    concluindo primeiro exercício

--- fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/exercicios.js
@@ -1,1 +1,10 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1 - Percorra o array imprimindo todos os valores contidos nele com a função console.log().
+
+for(i = 0; i < numbers.length; i += 1) {
+  const item = numbers[i];
+  console.log(item);
+}
+
+//2 - Some todos os valores contidos no array e imprima o resultado.
