81ff1d77699cb0231e0eb8ebc1e4fc60491a057f
vitor1532 <vitomartinsalmeida@gmail.com>
2023-07-13 15:25:52 -0300

    finalizando quinto requisito

--- fundamentos/secao-04-javascript-dom/dia-03-javascript-eventos/3-3/main.js
+++ fundamentos/secao-04-javascript-dom/dia-03-javascript-eventos/3-3/main.js
@@ -45,6 +45,14 @@ myWebpage.addEventListener('dblclick', (event) => {
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
 
+myWebpage.addEventListener('mouseover', (event) => {
+  event.target.style.color = 'rgb(124,252,0)';
+})
+
+myWebpage.addEventListener('mouseleave', (event) => {
+  event.target.style.color = 'unset';
+})
+
 // Segue abaixo um exemplo de uso do event.target.
 
 const resetText = (event) => {
