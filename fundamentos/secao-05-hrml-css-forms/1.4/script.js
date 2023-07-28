1a8d54572ab8d9421a03158d2a317d70110c28ed
vitor1532 <vitomartinsalmeida@gmail.com>
2023-07-28 17:04:27 -0300

    exercício 1

---
+++ fundamentos/secao-05-hrml-css-forms/1.4/script.js
@@ -0,0 +1,8 @@
+// SELETORES
+const inputText = document.querySelector("#input-text");
+const inputCheckbox = document.querySelector("#input-checkbox");
+const hrefLink = document.querySelector("#href");
+
+hrefLink.addEventListener('click', (event) => {
+  event.preventDefault();
+})
