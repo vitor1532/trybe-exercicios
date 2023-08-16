7fe66cce4c86b67bf4957b637357f5e48708034d
vitor1532 <vitomartinsalmeida@gmail.com>
2023-08-16 16:41:45 -0300

    instalando jest e colocando base inicial do primeiro exercício

---
+++ fundamentos/secao-06-es6-e-jest/6.2/removeItem.test.js
@@ -0,0 +1,14 @@
+const removeItem = require('./removeItem');
+
+describe('Testa a função removeItem', () => {
+  
+});
+
+// describe('testa função multiply', () => {
+//   test('retorna multiplicação com números positivos', () => {
+//     expect(multiply(10, 4)).toBe(40);
+//   });
+//   test('retorna multiplicação com número negativo', () => {
+//     expect(multiply(10, -4)).toBe(-40);
+//   });
+// })
