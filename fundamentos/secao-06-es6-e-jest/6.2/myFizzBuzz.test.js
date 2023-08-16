40b03f72e42615676f8bf81c49a8d64a0af3c6db
vitor1532 <vitomartinsalmeida@gmail.com>
2023-08-16 16:53:35 -0300

    iniciando segundo exercício

---
+++ fundamentos/secao-06-es6-e-jest/6.2/myFizzBuzz.test.js
@@ -0,0 +1,10 @@
+
+
+// describe('testa função multiply', () => {
+//   test('retorna multiplicação com números positivos', () => {
+//     expect(multiply(10, 4)).toBe(40);
+//   });
+//   test('retorna multiplicação com número negativo', () => {
+//     expect(multiply(10, -4)).toBe(-40);
+//   });
+// })
--- fundamentos/secao-06-es6-e-jest/6.2/removeItem.test.js
+++ fundamentos/secao-06-es6-e-jest/6.2/removeItem.test.js
@@ -11,12 +11,3 @@ describe('Testa a função removeItem', () => {
     expect(removeItem([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
-
-// describe('testa função multiply', () => {
-//   test('retorna multiplicação com números positivos', () => {
-//     expect(multiply(10, 4)).toBe(40);
-//   });
-//   test('retorna multiplicação com número negativo', () => {
-//     expect(multiply(10, -4)).toBe(-40);
-//   });
-// })
