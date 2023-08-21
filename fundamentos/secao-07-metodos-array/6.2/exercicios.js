f2a5a75af1ee89d155a99a2ba7e545307a74cdc0
vitor1532 <vitomartinsalmeida@gmail.com>
2023-08-21 16:40:58 -0300

    finalizando primeiro exercício

--- fundamentos/secao-07-metodos-array/6.2/exercicios.js
+++ fundamentos/secao-07-metodos-array/6.2/exercicios.js
@@ -1,1 +1,18 @@
 const pokemons = require('./pokemons');
+
+// Exercício 1
+// Utilizando o método filter e object destructuring, crie uma função filterStrongPokemons que retorna um novo array contendo apenas os Pokémons com nível superior a 30.
+
+const filterStrongPokemons = pokemons.filter(({ level }) => level > 30);
+
+console.log(filterStrongPokemons);
+
+// Exercício 2
+// Utilizando os métodos filter e map encadeados, crie uma função getStrongPokemonNames que retorna um novo array contendo apenas os nomes dos Pokémons com nível superior a 50.
+
+
+// Exercício 3
+// Utilize o método sort para ordenar o array de Pokémons em ordem crescente com base no nível de cada Pokémon.
+
+// Exercício 4
+// Utilizando o método sort e default destructuring, crie uma função chamada sortPokemons que retorna o array de Pokémons ordenado por nível em ordem decrescente. Caso dois Pokémons tenham o mesmo nível, a ordenação deve ser feita pelo nome em ordem alfabética crescente.
