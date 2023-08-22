6c6f342806d2b5787bca57a05fd30fc50925564e
vitor1532 <vitomartinsalmeida@gmail.com>
2023-08-22 11:43:26 -0300

    finalizando exercício 2

--- fundamentos/secao-07-metodos-array/7.3/exercicios.js
+++ fundamentos/secao-07-metodos-array/7.3/exercicios.js
@@ -10,4 +10,34 @@ const addPlanet = () => {
 
 addPlanet();
 
-console.log(countries);
+// console.log(countries);
+
+// Exercício 2
+// Para cada país, crie um objeto apenas com as informação do nome, código da moeda e nome da moeda. Essas informações devem ser armazenadas em um novo array. Faça esse exercício usando o método forEach, não vale map. 😜
+
+// Exemplo de como devem ser os objetos:
+
+// {
+//   name: 'United States',
+//   currencyCode: 'USD',
+//   currencyName: 'United States Dollar',
+// }
+
+
+const createObj = () => {
+  let newArray = [];
+  countries.forEach((element) => {
+    let newObj = {};
+
+    newObj = {
+      name: element.name,
+      currencyCode: element.currencies[0].code,
+      currancyName: element.currencies[0].name,
+    }
+    newArray.push(newObj);
+  });
+
+  return newArray;
+}
+
+console.log(createObj());
