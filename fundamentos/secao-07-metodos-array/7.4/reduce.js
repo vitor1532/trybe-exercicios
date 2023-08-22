b4c1af9b38827266bf5b880ce5dd99f7a6f7d4c3
vitor1532 <vitomartinsalmeida@gmail.com>
2023-08-22 14:36:33 -0300

    fazendo exercício de fixação 2

--- fundamentos/secao-07-metodos-array/7.4/reduce.js
+++ fundamentos/secao-07-metodos-array/7.4/reduce.js
@@ -1,3 +1,5 @@
+const students = require('./students')
+
 // Exercício 1
 // Faça uma função que some todos os números pares do array numbers usando reduce.
 
@@ -7,4 +9,31 @@ const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
 const sum = numbers.reduce((acc, current) => acc + current, 0);
 
-console.log(sum);
+// console.log(sum);
+
+// Exercício 2
+// Crie uma função usando dados de estudantes, para mostrar na tela um relatório que diz em qual matéria a pessoa foi melhor. Você usará tanto o map quanto o reduce dentro dele.
+// Resultado esperado: 
+// [
+//   { name: 'Jorge', course: 'Português' },
+//   { name: 'Mario', course: 'Biologia' },
+//   { name: 'Jorge', course: 'Português' },
+//   { name: 'Maria', course: 'Química' },
+//   { name: 'Natalia', course: 'Português' },
+//   { name: 'Wilson', course: 'Português' },
+// ]
+
+const report = (students) => students.map((student) => {
+  return {
+    name: student.name,
+    course: student.courses.reduce((acc, current) => {
+      if (acc.grade > current.grade) {
+        return acc;
+      } else {
+        return current;
+      }
+    }).name
+  }
+});
+
+console.log(report(students));
---
+++ fundamentos/secao-07-metodos-array/7.4/students.js
@@ -0,0 +1,76 @@
+const students = [
+  {
+    name: 'Jorge',
+    lastName: 'Silva',
+    age: 14,
+    turn: 'Manhã',
+    courses: [
+      { name: 'Matemática', grade: 67 },
+      { name: 'Português', grade: 79 },
+      { name: 'Química', grade: 70 },
+      { name: 'Biologia', grade: 65 },
+    ],
+  },
+  {
+    name: 'Mario',
+    lastName: 'Ferreira',
+    age: 15,
+    turn: 'Tarde',
+    courses: [
+      { name: 'Matemática', grade: 59 },
+      { name: 'Português', grade: 80 },
+      { name: 'Química', grade: 78 },
+      { name: 'Biologia', grade: 92 },
+    ],
+  },
+  {
+    name: 'Jorge',
+    lastName: 'Santos',
+    age: 15,
+    turn: 'Manhã',
+    courses: [
+      { name: 'Matemática', grade: 76 },
+      { name: 'Português', grade: 90 },
+      { name: 'Química', grade: 70 },
+      { name: 'Biologia', grade: 80 },
+    ],
+  },
+  {
+    name: 'Maria',
+    lastName: 'Silveira',
+    age: 14,
+    turn: 'Manhã',
+    courses: [
+      { name: 'Matemática', grade: 91 },
+      { name: 'Português', grade: 85 },
+      { name: 'Química', grade: 92 },
+      { name: 'Biologia', grade: 90 },
+    ],
+  },
+  {
+    name: 'Natalia',
+    lastName: 'Castro',
+    age: 14,
+    turn: 'Manhã',
+    courses: [
+      { name: 'Matemática', grade: 70 },
+      { name: 'Português', grade: 70 },
+      { name: 'Química', grade: 60 },
+      { name: 'Biologia', grade: 50 },
+    ],
+  },
+  {
+    name: 'Wilson',
+    lastName: 'Martins',
+    age: 14,
+    turn: 'Manhã',
+    courses: [
+      { name: 'Matemática', grade: 80 },
+      { name: 'Português', grade: 82 },
+      { name: 'Química', grade: 79 },
+      { name: 'Biologia', grade: 75 },
+    ],
+  },
+];
+
+module.exports = students;
