818866a7585863fa50fab60009bae790ecc865c2
vitor1532 <vitomartinsalmeida@gmail.com>
2023-08-22 13:10:41 -0300

    finalizando exercício 5

--- fundamentos/secao-07-metodos-array/7.3/exercicios.js
+++ fundamentos/secao-07-metodos-array/7.3/exercicios.js
@@ -72,3 +72,30 @@ const newCountry = {
 const addCountry = (newCountry, countries) => [...countries, newCountry];
 
 // console.log(addCountry(newCountry, countries));
+
+// Exercício 5
+// Desenvolva uma solução para percorrer o array countries e construir uma string para cada país com informações e formatação específicas. As propriedades são passadas como argumentos para a função buildString e adicionadas à string final. A string deverá ser construída com as propriedades name, capital, currencies.code e currencies.name separadas por um -.
+
+// O resultado final deve ser:
+
+// Anguilla - The Valley - XCD - East Caribbean dollar
+// Brazil - Brasília - BRL - Brazilian Real
+// Canada - Ottawa - CAD - Canadian Dollar
+// China - Beijing - CNY - Chinese Yuan
+// France - Paris - EUR - Euro
+// India - New Delhi - INR - Indian Rupee
+// Mexico - Mexico City - MXN - Mexican Peso
+// Russia - Moscow - RUB - Russian Ruble
+// South Africa - Pretoria - ZAR - South African Rand
+// United States - Washington -  D.C. - USD - United States Dollar
+
+const buildString = (countries, name, ...propreties) => {
+  const found = countries.find((country) => country.name === name);
+
+  return `${found.name} - ${propreties}`;
+}
+
+countries.forEach((element) => {
+  const result = buildString(countries, element.name, element.capital, element.currencies[0].code, element.currencies[0].name);
+  console.log(result.split(',').join(' - '));
+})
